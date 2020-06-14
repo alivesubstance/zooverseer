32a66d4fe76089499848dd186bed4e218adcb9fb
mirian <mirian@pleeco.com>
2020-06-14 22:35:23 +0300

    fix build linux script

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -6,13 +6,14 @@ GOARCH="amd64"
 export NO_AT_BRIDGE=1
 
 #PATH="${PATH}:./bin"
-ASSETS_DIR="../assets"
+SRC_ASSETS_DIR="assets"
 #BIN_DIR="../bin"
 INSTALL_DIR="../build/linux-1.0.0b"
+INSTALL_ASSETS_DIR=${INSTALL_DIR}/${SRC_ASSETS_DIR}
 
 rm -rf "${INSTALL_DIR}"
-mkdir -p "${INSTALL_DIR}/conf"
-cp "${ASSETS_DIR}/main.glade" "${INSTALL_DIR}/conf/main.glade"
-cp "${ASSETS_DIR}/style.css" "${INSTALL_DIR}/conf/style.css"
+mkdir -p "${INSTALL_ASSETS_DIR}"
+cp "../${SRC_ASSETS_DIR}/main.glade" "${INSTALL_ASSETS_DIR}/main.glade"
+cp "../${SRC_ASSETS_DIR}/style.css" "${INSTALL_ASSETS_DIR}/style.css"
 
 env GOOS="${GOOS}" GOARCH="${GOARCH}" go build -o "${INSTALL_DIR}/zooverseer" github.com/alivesubstance/zooverseer/cmd
