84e71fb0ddd67b8fb67e61546bb62de3a32ad016
mirian <mirian@pleeco.com>
2020-07-06 23:49:47 +0300

    build scripts

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-GOOS="linux"
+GOOS="darwin"
 GOARCH="amd64"
 
+export NO_AT_BRIDGE=1
 
 #PATH="${PATH}:./bin"
 SRC_ASSETS_DIR="assets"
 #BIN_DIR="../bin"
-INSTALL_DIR="../bin/linux-1.0.0b"
+INSTALL_DIR="../bin/mac-1.0.0b"
 INSTALL_ASSETS_DIR=${INSTALL_DIR}/${SRC_ASSETS_DIR}
 
 rm -rf "${INSTALL_DIR}"
@@ -17,10 +18,4 @@ cp "../${SRC_ASSETS_DIR}/logo.png" "${INSTALL_ASSETS_DIR}/logo.png"
 cp "../${SRC_ASSETS_DIR}/logo_small.png" "${INSTALL_ASSETS_DIR}/logo_small.png"
 cp "../${SRC_ASSETS_DIR}/style.css" "${INSTALL_ASSETS_DIR}/style.css"
 
-env \
-  GOOS="${GOOS}" \
-  GOARCH="${GOARCH}" \
-  NO_AT_BRIDGE=1 \
-  # include gdk.go.h into build.
-  CGO_CFLAGS="-I/home/mirian/code/go/src/github.com/gotk3/gotk3/gdk" \
-  go build -o "${INSTALL_DIR}/zooverseer" github.com/alivesubstance/zooverseer/cmd
+env GOOS="${GOOS}" GOARCH="${GOARCH}" go build -o "${INSTALL_DIR}/zooverseer" github.com/alivesubstance/zooverseer/cmd
--- scripts/build_win.sh
+++ scripts/build_win.sh
@@ -3,4 +3,21 @@
 GOOS="windows"
 GOARCH="amd64"
 
-env GOOS="${GOOS}" GOARCH="${GOARCH}" go build -o /home/mirian/bin/zooverseer.exe github.com/alivesubstance/zooverseer/cmd
+#PATH="${PATH}:./bin"
+SRC_ASSETS_DIR="assets"
+#BIN_DIR="../bin"
+INSTALL_DIR="../bin/win-1.0.0b"
+INSTALL_ASSETS_DIR=${INSTALL_DIR}/${SRC_ASSETS_DIR}
+
+rm -rf "${INSTALL_DIR}"
+mkdir -p "${INSTALL_ASSETS_DIR}"
+cp "../${SRC_ASSETS_DIR}/main.glade" "${INSTALL_ASSETS_DIR}/main.glade"
+cp "../${SRC_ASSETS_DIR}/logo.png" "${INSTALL_ASSETS_DIR}/logo.png"
+cp "../${SRC_ASSETS_DIR}/logo_small.png" "${INSTALL_ASSETS_DIR}/logo_small.png"
+cp "../${SRC_ASSETS_DIR}/style.css" "${INSTALL_ASSETS_DIR}/style.css"
+
+GOOS="${GOOS}" GOARCH="${GOARCH}" \
+  CGO_ENABLED=1 CXX=i686-w64-mingw32-g++ CC=i686-w64-mingw32-gcc \
+  go build -o "${INSTALL_DIR}/zooverseer.exe" \
+  github.com/alivesubstance/zooverseer/cmd
+
