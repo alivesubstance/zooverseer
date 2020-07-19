cbbc69716f11d192b9530a9e45e6c96a5c184e79
Mirian Dzhachvadze <alivesubstance@gmail.com>
2020-07-19 23:57:35 +0300

    build_win.sh that builds correct executable

--- scripts/build_win.sh
+++ scripts/build_win.sh
@@ -16,13 +16,7 @@ cp "../${SRC_ASSETS_DIR}/logo_small.png" "${INSTALL_ASSETS_DIR}/logo_small.png"
 cp "../${SRC_ASSETS_DIR}/style.css" "${INSTALL_ASSETS_DIR}/style.css"
 
 GOOS="${GOOS}" GOARCH="${GOARCH}" \
-#  CGO_ENABLED=1 CXX=i686-w64-mingw32-g++ CC=i686-w64-mingw32-gcc \
-#  CGO_ENABLED=1 CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc \
-#  NO_AT_BRIDGE=1 \
-#  CGO_CFLAGS="-IC:\Users\Professional\go\src\github.com\gotk3\gotk3\gdk" \
-#  CGO_CFLAGS="-LC:\code\zooverseer\scripts\win\dll" \
+  CGO_ENABLED=1 CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc \
+  NO_AT_BRIDGE=1 \
   go build -o "${INSTALL_DIR}/zooverseer.exe" \
   github.com/alivesubstance/zooverseer/cmd
-
-
-#GOOS="windows" GOARCH="amd64" go build -o "${INSTALL_DIR}/app.exe" github.com/app/cmd
